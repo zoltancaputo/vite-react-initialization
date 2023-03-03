dc469ed74e2b06fae4f17edc0b38c9cddd22dbd0
Zoltan <zoltancaputo@gmail.com.br>
2023-03-03 15:53:13 -0300

    how to start vite

---
+++ README.md
@@ -0,0 +1,5 @@
+Welcome to this repository.
+I realy recommend Rockeatseat platform to have more understanding about of the project for beginners.
+It is just a simple start remembering how to initiate the project.
+Here we have specified packages that we are using.
+We also learn through the json package which dependencies wore installed.
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {Post} from './Post';
+
+export function App() {
+
+  return (
+
+    <div>
+      <Post 
+        author="Zoltan Caputo" 
+        content="Lorem ipsum sdfasdfasdfasdfasdfasdfasdfasdf "
+      />
+      <Post 
+        author="Cica Nários" 
+        content="Lorem ipsaaaaaaa chachá sdfasdfasdfasdfasdfasdfasdfasdf "
+      />
+    </div>
+      
+  )
+}
+
+
